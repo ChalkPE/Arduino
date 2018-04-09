d3802bbb33002136654c7329cd97d288d79924b3
Chalk <chalkpe@gmail.com>
2018-04-10 07:39:41 +0900

    remove unused files

--- sketch_apr6a/sketch_apr6a.ino
+++ sketch_apr6a/sketch_apr6a.ino
@@ -1,1 +1,18 @@
-#include <SimpleTimer.h>const int LED = 8;SimpleTimer timer;void setup() {  pinMode(LED, OUTPUT);  timer.setInterval(1000, wow);}void loop() {  timer.run();}void wow() {  static bool led = true;  digitalWrite(LED, led = !led);}
+#include <SimpleTimer.h>
+
+const int LED = 8;
+SimpleTimer timer;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  timer.setInterval(1000, wow);
+}
+
+void loop() {
+  timer.run();
+}
+
+void wow() {
+  static bool led = true;
+  digitalWrite(LED, led = !led);
+}
